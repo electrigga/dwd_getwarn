e9be4d3c2dc1ce55272595a8de3b68dda8e7ff1f
Jens <electrigga@gmx.net>
2017-07-10 11:14:59 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$ftp_server = "ftp-outgoing2.dwd.de";
+$ftp_user_name = "user";
+$ftp_user_pass = "pass";
+$ftp_verzeichnis_sms = "gds/specials/alerts/sms/LZ/";
+$ftp_verzeichnis_cap = "gds/specials/alerts/cap/LZ/";
+?>
